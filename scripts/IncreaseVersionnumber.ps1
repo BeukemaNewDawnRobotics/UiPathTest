1f1c91ca0234f29ccbce61f0aa3bf279599afdaf
BeukemaNewDawnRobotics <131771479+BeukemaNewDawnRobotics@users.noreply.github.com>
2023-05-30 16:15:45 +0200

    Update IncreaseVersionnumber.ps1

--- scripts/IncreaseVersionnumber.ps1
+++ scripts/IncreaseVersionnumber.ps1
@@ -8,8 +8,8 @@ Param (
 $pathToJson = 'project.json'
 $a = Get-Content 'project.json' -raw | ConvertFrom-Json
 $version = [version]($a).projectVersion
-Write-Host "Versie " $version " verhoogd naar " $newVersion
 $newVersion = "{0}.{1}.{2}" -f $version.Major, $version.Minor, ($version.Build+1)
+Write-Host "Versie " $version " verhoogd naar " $newVersion
 $a.projectVersion=$newVersion
 $a | ConvertTo-Json | set-content $pathToJson
 Write-Host "Nieuwe versie weggeschreven in de JSON"
