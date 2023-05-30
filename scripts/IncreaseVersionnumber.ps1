2c4a317f54067fba5f0825a2d95761d6deb684ff
Sander <sbeukema@newdawnrobotics.nl>
2023-05-30 16:42:33 +0200

    test commit

--- scripts/IncreaseVersionnumber.ps1
+++ scripts/IncreaseVersionnumber.ps1
@@ -11,5 +11,5 @@ $version = [version]($a).projectVersion
 $newVersion = "{0}.{1}.{2}" -f $version.Major, $version.Minor, ($version.Build+1)
 Write-Host "Versie " $version " verhoogd naar " $newVersion
 $a.projectVersion=$newVersion
-$a | ConvertTo-Json | set-content $pathToJson
+$a | ConvertTo-Json -Depth 4| set-content $pathToJson 
 Write-Host "Nieuwe versie weggeschreven in de JSON"
